3e4b7a15d5c6ae573d2a03b50892a1700988918e
lirone <lironesamoun@flod.ai>
2019-11-06 00:05:33 +0100

    initial version graphis

--- index.php
+++ index.php
@@ -44,146 +44,166 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     $dbName = "sentinel_test";
     $db = new DB($userName, $password, $dbName,$host);
 
-    $all_id_sensors = $db->query_select('SELECT id FROM `sensor` WHERE 1',"id");
-    $all_site = $db->query_select_light('SELECT id, nom FROM site WHERE owner_id='.$_SESSION['id']);
-    $all_equipment = $db->query_select('SELECT nom FROM `structure` WHERE 1',"nom");
-    $min_max_date_record = $db->query('SELECT (SELECT Max(date_time) FROM record) AS MaxDateTime,
-    (SELECT Min(date_time) FROM record) AS MinDateTime');
-    $min_date_time = $min_max_date_record[0]->MinDateTime;
-    $max_date_time = $min_max_date_record[0]->MaxDateTime;
-    $min_date_time = strtotime( $min_date_time );
-    $max_date_time = strtotime( $max_date_time );
-    $min_date = date( 'm-d-Y', $min_date_time );
-    $max_date = date( 'm-d-Y', $max_date_time );
+    $session_id = $_SESSION['id'];
+    $query_select_site = "SELECT DISTINCT site, site_id FROM (SELECT site.nom AS site , site.id AS site_id, gn.name AS GroupeName FROM structure AS st
+      LEFT JOIN record AS r ON (r.structure_id=st.id)
+      LEFT JOIN sensor AS s ON (s.id = r.sensor_id)
+      LEFT JOIN sensor_group AS gs ON (gs.sensor_id=s.id)
+      LEFT JOIN group_name AS gn ON (gn.group_id = gs.groupe_id)
+      LEFT JOIN group_site AS grs ON (grs.group_id=gn.group_id)
+      LEFT JOIN site ON (site.id = grs.site_id)
+      WHERE gn.name LIKE 'RTE' AND owner_id=$session_id) AS RTE";
 
-    ?>
+      $query_select_equipement = "SELECT DISTINCT equipement, equipement_id FROM (SELECT site.nom AS site ,st.nom AS equipement, st.id AS equipement_id, gn.name AS GroupeName FROM structure AS st
+        LEFT JOIN record AS r ON (r.structure_id=st.id)
+        LEFT JOIN sensor AS s ON (s.id = r.sensor_id)
+        LEFT JOIN sensor_group AS gs ON (gs.sensor_id=s.id)
+        LEFT JOIN group_name AS gn ON (gn.group_id = gs.groupe_id)
+        LEFT JOIN group_site AS grs ON (grs.group_id=gn.group_id)
+        LEFT JOIN site ON (site.id = grs.site_id)
+        WHERE gn.name LIKE 'RTE') AS RTE";
 
-    <div class="container">
-    </br>
-    <div class="col text-center">
-      <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
-    </div>
-  </br>
-  <form name="contact-form" class="ui tiny form" action="" method="post" id="sendData">
+        $all_site = $db->query_select_light($query_select_site);
 
-    <div class="ui form">
-      <div class="four fields">
-        <div class="field">
-          <label>Choose site</label>
-          <select class="browser-default custom-select" name="siteDB" id="siteDB">
-            <option value="" selected>Select site</option>
-            <?php while($site = $all_site->fetch_object()){
-              echo "<option value='$site->id'>$site->nom </option>";
-            }
-            ?>
-          </select>
+        $all_equipment = $db->query_select_light($query_select_equipement);
+
+        $min_max_date_record = $db->query('SELECT (SELECT Max(date_time) FROM record) AS MaxDateTime,
+        (SELECT Min(date_time) FROM record) AS MinDateTime');
+        $min_date_time = $min_max_date_record[0]->MinDateTime;
+        $max_date_time = $min_max_date_record[0]->MaxDateTime;
+        $min_date_time = strtotime( $min_date_time );
+        $max_date_time = strtotime( $max_date_time );
+        $min_date = date( 'm-d-Y', $min_date_time );
+        $max_date = date( 'm-d-Y', $max_date_time );
+
+        ?>
+
+        <div class="container">
+        </br>
+        <div class="col text-center">
+          <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
         </div>
+      </br>
+      <form name="contact-form" class="ui tiny form" action="" method="post" id="sendData">
 
-        <div class="field" id="equipmentField">
-          <label>Choose equipment</label>
-          <select class="browser-default custom-select" name="equipment"  id="equipment">
-            <option value="" selected>Select Equipement</option>
-            <?php foreach($all_equipment as $equipment){
-              echo "<option value='$equipment'>$equipment</option>";
-            }
-            ?>
-          </select>
-        </div>
-
-        <div class="field">
-          <label>Start date</label>
-          <div class="ui calendar" id="startDate">
-            <div class="ui input left icon">
-              <i class="calendar icon"></i>
-              <input type="text" placeholder="Date">
+        <div class="ui form">
+          <div class="four fields">
+            <div class="field">
+              <label>Choose site</label>
+              <select class="browser-default custom-select" name="siteDB" id="siteDB">
+                <option value="" selected>Select site</option>
+                <?php while($site = $all_site->fetch_object()){
+                  echo "<option value='$site->site_id'>$site->site </option>";
+                }
+                ?>
+              </select>
             </div>
+
+            <div class="field" id="equipmentField">
+              <label>Choose equipment</label>
+              <select class="browser-default custom-select" name="equipment"  id="equipment">
+                <option value="" selected>Select Equipement</option>
+                <?php while($equipment = $all_equipment->fetch_object()){
+                  echo "<option value='$equipment->equipement_id'>$equipment->equipement</option>";
+                }
+                ?>
+              </select>
+            </div>
+
+            <div class="field">
+              <label>Start date</label>
+              <div class="ui calendar" id="startDate">
+                <div class="ui input left icon">
+                  <i class="calendar icon"></i>
+                  <input type="text" placeholder="Date">
+                </div>
+              </div>
+            </div>
+
+            <div class="field">
+              <label>End date</label>
+              <div class="ui calendar" id="endDate">
+                <div class="ui input left icon">
+                  <i class="calendar icon"></i>
+                  <input type="text" placeholder="Date">
+                </div>
+              </div>
+            </div>
+
+            <div class="field">
+              <label>Choose type</label>
+              <select class="browser-default custom-select" id="typemsg">
+                <option value="" selected>Select type</option>
+                <option value=''>All</option>
+                <option value='choc'>Choc</option>
+                <option value='inclinometre'>Inclinometre</option>
+                <option value='global'>Global</option>
+              </select>
+            </div>
+
+            <button type="submit" id="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+        <div class="form-check">
+          <div class="custom-control custom-checkbox">
+            <input type="checkbox" class="custom-control-input" name ="groupby" id="groupby">
+            <label class="custom-control-label" for="groupby">Group Result</label>
           </div>
         </div>
-
-        <div class="field">
-          <label>End date</label>
-          <div class="ui calendar" id="endDate">
-            <div class="ui input left icon">
-              <i class="calendar icon"></i>
-              <input type="text" placeholder="Date">
-            </div>
+        <div class="form-check">
+          <!-- Default checked -->
+          <div class="custom-control custom-checkbox">
+            <input type="checkbox" class="custom-control-input" name ="allmsg" id="allmsg" checked>
+            <label class="custom-control-label" for="allmsg">All messages</label>
           </div>
         </div>
+      </div>
 
-        <div class="field">
-          <label>Choose type</label>
-          <select class="browser-default custom-select" id="typemsg">
-            <option value="" selected>Select type</option>
-            <option value=''>All</option>
-            <option value='choc'>Choc</option>
-            <option value='inclinometre'>Inclinometre</option>
-            <option value='global'>Global</option>
-          </select>
+
+      <!--
+      <div style="width: 50%">
+      <canvas id="canvas2" height="450" width="600"></canvas>
+    </div>
+  -->
+  <div id="chart-display-container-specific" class="container">
+    <canvas id="canvas" height="450" width="1200"></canvas>
+  </div>
+  <div id="chart-display-container-all" class="container">
+    <div class="row">
+      <div class="col">
+        <div id="chart-container">
+          <canvas id="canvas_spectre" height="450" width="1200"></canvas>
         </div>
-
-        <button type="submit" id="submit" class="btn btn-primary">Submit</button>
-      </form>
-    </div>
-    <div class="form-check">
-      <div class="custom-control custom-checkbox">
-        <input type="checkbox" class="custom-control-input" name ="groupby" id="groupby">
-        <label class="custom-control-label" for="groupby">Group Result</label>
       </div>
     </div>
-    <div class="form-check">
-      <!-- Default checked -->
-      <div class="custom-control custom-checkbox">
-        <input type="checkbox" class="custom-control-input" name ="allmsg" id="allmsg" checked>
-        <label class="custom-control-label" for="allmsg">All messages</label>
+    <div class="row">
+      <div class="col">
+        <div id="chart-container">
+          <canvas id="canvas_inclinometre"></canvas>
+        </div>
+      </div>
+      <div class="col">
+        <div id="chart-container">
+          <canvas id="canvas_temperature"></canvas>
+        </div>
       </div>
     </div>
-  </div>
-
-
-  <!--
-  <div style="width: 50%">
-  <canvas id="canvas2" height="450" width="600"></canvas>
-</div>
--->
-<div id="chart-display-container-specific" class="container">
-  <canvas id="canvas" height="450" width="1200"></canvas>
-</div>
-<div id="chart-display-container-all" class="container">
-  <div class="row">
-    <div class="col">
-      <div id="chart-container">
-        <canvas id="canvas_spectre" height="450" width="1200"></canvas>
+    <div class="row">
+      <div class="col">
+      </div>
+      <div class="col">
+        <div id="chart-container">
+          <canvas id="canvas_choc"></canvas>
+        </div>
+      </div>
+      <div class="col">
       </div>
     </div>
   </div>
-  <div class="row">
-    <div class="col">
-      <div id="chart-container">
-        <canvas id="canvas_inclinometre"></canvas>
-      </div>
-    </div>
-    <div class="col">
-      <div id="chart-container">
-        <canvas id="canvas_temperature"></canvas>
-      </div>
-    </div>
+  <!-- Container for displaying the table data -->
+  <div class="container">
+    <div id="resultcontainer"></div>
   </div>
-  <div class="row">
-    <div class="col">
-    </div>
-    <div class="col">
-      <div id="chart-container">
-        <canvas id="canvas_choc"></canvas>
-      </div>
-    </div>
-    <div class="col">
-    </div>
-  </div>
-</div>
-<!-- Container for displaying the table data -->
-<div class="container">
-  <div id="resultcontainer"></div>
-</div>
 </body>
 
 <script>
@@ -234,67 +254,6 @@ $(document).on("click", "a.download", function(e) {
     }
   });
 });
-
-function drawTemperature(data){
-  console.log(data);
-  data = JSON.parse(data);
-
-  var temperature = [];
-  var date = [];
-  for (var i in data) {
-    temperature.push(data[i].temperature);
-    date.push(data[i].date_d);
-  }
-
-  console.log(date);
-  var chartdata = {
-    labels: date,
-    datasets : [
-      {
-        labels: date,
-        borderColor: "#3e95cd",
-        backgroundColor: "#f6f6f6",
-        pointBackgroundColor: "#3e95cd",
-        hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
-        hoverBorderColor: 'rgba(200, 200, 200, 1)',
-        data: temperature
-      }
-    ]
-  };
-  var ctx = $("#canvas");
-  var timeFormat = 'YYYY-MM-DD';
-  var barGraph = new Chart(ctx, {
-    type: 'line',
-    data: chartdata,
-
-    options: {
-      scales: {
-        xAxes: [{
-          scaleLabel: {
-            display: true,
-            labelString: 'Date'
-          }
-        }],
-        yAxes: [{
-          ticks: {
-            beginAtZero: false,
-          },
-          scaleLabel: {
-            display: true,
-            labelString: 'Temeprature (°C)'
-          },
-          //type: 'logarithmic',
-        }]
-      },
-      legend: { display: false },
-      title: {
-        display: true,
-        text: 'Temperature en fonction du temps'
-      }
-    }
-  });
-}
-
 
 function drawTemperatureFromData(temperatureDataJson, canvaID = "canvas_temperature"){
   if (typeof temperatureDataJson != 'object'){
@@ -609,155 +568,7 @@ function drawChocFromData(chocDataJson, canvaID = "canvas_choc" ){
   });
 }
 
-function drawInclinometer(data){
-  //console.log(data);
-  data = JSON.parse(data);
-  var nx = [];
-  var ny = [];
-  var nz = [];
-  var date = [];
-  for (var i in data) {
-    nx.push(data[i].nx);
-    ny.push(data[i].ny);
-    nz.push(data[i].nz);
-    date.push(data[i].date_d);
-  }
-
-  //console.log(date);
-  var chartdata = {
-    labels: date,
-    datasets : [
-      {
-        label: 'X',
-        fill: false,
-        backgroundColor: 'blue',
-        borderColor: 'blue',
-        data: nx
-      },
-      {
-        label: 'Y',
-        fill: false,
-        backgroundColor: 'orange',
-        borderColor: 'orange',
-        data: ny
-      },
-      {
-        label: 'Z',
-        fill: false,
-        backgroundColor: 'green',
-        borderColor: 'green',
-        data: nz
-      }
-    ]
-  };
-  var ctx = $("#canvas");
-  var timeFormat = 'YYYY-MM-DD';
-  var barGraph = new Chart(ctx, {
-    type: 'line',
-    data: chartdata,
-
-    options: {
-      scales: {
-        xAxes: [{
-          scaleLabel: {
-            display: true,
-            labelString: 'Date'
-          }
-        }],
-        yAxes: [{
-          ticks: {
-            beginAtZero: false,
-          },
-          scaleLabel: {
-            display: true,
-            labelString: 'Height (m)'
-          },
-          //type: 'logarithmic',
-        }]
-      },
-      legend: { display: true },
-      title: {
-        display: true,
-        text: 'Inclinometre en fonction du temps'
-      }
-    }
-  });
-}
-
-//Draw choc data
-function drawChoc(data){
-  data = JSON.parse(data);
-  var amplitude = [];
-  var amplitude_1 = [];
-  var amplitude_2 = [];
-  var time_data = [];
-  var time_1 = [];
-  var time_2 = [];
-  var date = []
-
-  amplitude.push(parseInt(data[1].amplitude_1));
-  amplitude.push(parseInt(data[1].amplitude_2));
-  time_data.push(parseInt(data[1].time_1));
-  time_data.push(parseInt(data[1].time_2));
-  var array_data = []; // create an empty array
-
-  var obj = {x:0,y:0};
-  array_data.push(obj);
-  for(var i=0;i<2;i++)
-  {
-    var obj = {x:time_data[i],y:amplitude[i]};
-    array_data.push(obj);
-  }
-  var obj = {x:time_data[0]+time_data[1],y:0};
-  array_data.push(obj);
-
-  var chartdata = {
-    labels: date,
-    datasets : [
-      {
-        data:array_data,
-        showLine: true,
-      }
-    ]
-  };
-  var ctx = $("#canvas");
-  var barGraph = new Chart(ctx, {
-    type: 'scatter',
-    data: chartdata,
-    options: {
-      scales: {
-        display : true,
-        xAxes: [{
-          ticks: {
-            beginAtZero: true,
-          },
-          scaleLabel: {
-            display: true,
-            labelString: 'Time (μs)'
-          }
-        }],
-        yAxes: [{
-          //reverse: true,
-          //  type:       "time",
-          ticks: {
-            beginAtZero: true,
-          },
-          scaleLabel: {
-            display: true,
-            labelString: 'Amplitude (mg)'
-          }
-        }]
-      },
-      legend: { display: false },
-      title: {
-        display: true,
-        text: 'Choc '
-      }
-    }
-  });
-}
 //Draw a specific subspectre from the sensor
-
 function drawSubSpectreFromData(subspectreDataJson,canvaID = "canvas_subspectre"){
   //console.log(data);
   if (typeof subspectreDataJson != 'object'){
@@ -833,102 +644,7 @@ function drawSubSpectreFromData(subspectreDataJson,canvaID = "canvas_subspectre"
   });
 }
 
-//Draw the completre spectrum (5 subspectre assembled)
-function drawCompleteSpectre(data){
-  //console.log(data);
-  data = JSON.parse(data);
-  var subspectre = [];
-  var minFreqData = [];
-  var maxFreqData = [];
-  var resolutionData = [];
-  var date_data = [];
-  for (var i in data) {
-    subspectre.push(data[i].subspectre);
-    minFreqData.push(parseInt(data[i].min_freq));
-    maxFreqData.push(parseInt(data[i].max_freq));
-    resolutionData.push(parseInt(data[i].resolution));
-    date_data.push(data[i].date_time);
-  }
 
-  min_freq = parseInt(minFreqData[0]);
-  var min_freq_initial = min_freq;
-  var max_freq = parseInt(maxFreqData[4]);
-  var index_start = 2;
-  var index_stop = 4;
-  var array_data = [];
-
-  for (var i = 0; i < subspectre.length; i++){
-    for (var j = 0; j < subspectre[i].length/2; j++){
-      var y_data_amplitude = hex2dec(subspectre[i].substring(index_start, index_stop))
-      if (j > 0){
-        min_freq = min_freq + resolutionData[i];
-      }
-      if (j < (subspectre[i].length/2)-1){
-        var obj = {x:min_freq,y:y_data_amplitude};
-        array_data.push(obj);
-      }
-      index_start+=2;
-      index_stop+=2;
-    }
-    index_start = 2;
-    index_stop = 4;
-  }
-  //console.log(array_data);
-  var title = "All spectre between " + date_data[0] + " and " + date_data[4];
-  var chartdata = {
-    datasets : [
-      {
-        data:array_data,
-        showLine: true,
-        borderColor: "#3e95cd",
-        backgroundColor: "#f6f6f6",
-        pointBackgroundColor: "#3e95cd",
-      }
-    ]
-  };
-  var ctx = $("#canvas");
-  var barGraph = new Chart(ctx, {
-    type: 'scatter',
-    data: chartdata,
-    options: {
-      scales: {
-        display : true,
-        xAxes: [{
-          gridLines: {
-            display:false
-          },
-          ticks: {
-            beginAtZero: false,
-            min: min_freq_initial,
-            max : max_freq,
-          },
-          scaleLabel: {
-            display: true,
-            labelString: 'Frequence (Hz)'
-          }
-        }],
-        yAxes: [{
-          gridLines: {
-            display:false
-          },
-          ticks: {
-            beginAtZero: false,
-          },
-          scaleLabel: {
-            display: true,
-            labelString: 'Amplitude (mg)'
-          },
-          //type: 'logarithmic',
-        }]
-      },
-      legend: { display: false },
-      title: {
-        display: true,
-        text: title
-      }
-    }
-  });
-}
 function hex2dec(hex){
   return parseInt(hex,16);
 }
