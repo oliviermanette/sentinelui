90e7bff44c37f84494ff982efea93ad07b872277
Lirone <lirone@flod.ai>
2019-12-24 13:14:57 +0100

    Add comment to function

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,4 +1,8 @@
-
+/**
+ * @desc remove specific element based on ID on html 
+ * @param string id - the class to remove in a specific attribute
+ * @return 
+ */
 function removeElement(id) {
   if(document.getElementById(id)){
     var elem = document.getElementById(id);
@@ -6,6 +10,11 @@ function removeElement(id) {
   }
 }
 
+/**
+ * @desc remove specific element and all the childs based on ID on html 
+ * @param string id - the class to remove in a specific attribute
+ * @return 
+ */
 function removeElementAndChilds(id){
   const parent = document.getElementById(id);
   while (parent.firstChild) {
@@ -13,7 +22,13 @@ function removeElementAndChilds(id){
   }
 }
 
+/**
+ * @desc format a date to the following format DD/MM/YYYY
+ * @param Date date - date to change format
+ * @return string new format 
+ */
 function getFormattedDate(date) {
+
   var year = date.getFullYear();
 
   var month = (1 + date.getMonth()).toString();
@@ -25,7 +40,13 @@ function getFormattedDate(date) {
   return day + '/' + month + '/' + year;
 }
 
-
+/**
+ * @desc add a new element in html page 
+ * @param string parentId - parent class where we want to insert new attribute
+ * @param string elementTag - type of attribute to add (div, h1...)
+ * @param string elementId - class for this specific attribute
+ * @return 
+ */
 function addElement(parentId, elementTag, elementId)
 {
   var p = document.getElementById(parentId);
@@ -34,17 +55,31 @@ function addElement(parentId, elementTag, elementId)
   p.appendChild(newElement);
 }
 
+/**
+ * @desc show specific element in html page if it's hidden
+ * @param string parentId - class we want to see
+ * @return 
+ */
 function showElement(parentId){
   var p = document.getElementById(parentId);
   p.style.display = "block";
 }
 
-
+/**
+ * @desc hide specific element in html page if it's shown
+ * @param string parentId - class we want to hide
+ * @return 
+ */
 function hideElement(parentId){
   var p = document.getElementById(parentId);
   p.style.display = "none";
 }
 
+/**
+ * @desc convert hex to base 10 decimal
+ * @param string hex - hex string to convert
+ * @return 
+ */
 function hex2dec(hex){
   return parseInt(hex,16);
 }
