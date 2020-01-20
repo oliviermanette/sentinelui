8ad25e234651e1fbe9bb8715d50d4343681a90db
Lirone <lirone@flod.ai>
2020-01-20 19:44:54 +0100

    Add function to TimeSeriesMAanger to get specific timeseries data

--- App/Models/TimeSeriesManager.php
+++ App/Models/TimeSeriesManager.php
@@ -20,11 +20,63 @@ class TimeSeriesManager extends \Core\Model
     }
 
     public function getAllTimeSeries(){
+        $db = static::getDB();
 
+        $sql = "select * FROM timeseries";
+
+        $stmt = $db->prepare($sql);
+
+        if ($stmt->execute()) {
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $results;
+        }
     }
 
-    public function getSpecificTimeSeries(){
-        
+    public function getSpecificTimeSeriesFromPool($pool_id, $date_time = ""){
+        $db = static::getDB();
+
+        $sql = "SELECT p.id AS pool_id, Date(timeseries.date_time) AS date, valueX, valueY 
+        FROM `timeseries` 
+        LEFT JOIN record AS r ON (r.id = timeseries.record_id)
+        LEFT JOIN sensor AS s ON (s.id = r.sensor_id)
+        LEFT JOIN pool AS p ON (p.sensor_id=s.id)
+        WHERE p.id = :pool_id
+        AND DATE(timeseries.date_time) LIKE '%:date_time%'";
+
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':pool_id', $pool_id, PDO::PARAM_INT);
+        $stmt->bindValue(':date_time', $pool_id, PDO::PARAM_STR);
+        $stmt->bindValue(':date_time', $date_time, PDO::PARAM_STR);
+
+        if ($stmt->execute()) {
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $results;
+        }
+ 
+    }
+
+    public function getSpecificTimeSeriesFromSensorID($sensor_id, $structure_id, $date_time = "")
+    {
+        $db = static::getDB();
+
+        $sql = "SELECT p.id AS pool_id, timeseries.sensor_id AS sensor_id, timeseries.structure_id AS structure_id, Date(timeseries.date_time) AS date, valueX, valueY 
+        FROM `timeseries` 
+        LEFT JOIN record AS r ON (r.id = timeseries.record_id)
+        LEFT JOIN sensor AS s ON (s.id = r.sensor_id)
+        LEFT JOIN pool AS p ON (p.sensor_id=s.id)
+        WHERE timeseries.sensor_id = :sensor_id 
+        AND timeseries.structure_id = : 
+        AND DATE(timeseries.date_time) LIKE '%:date_time%'";
+
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':sensor_id', $sensor_id, PDO::PARAM_INT);
+        $stmt->bindValue(':structure_id', $structure_id, PDO::PARAM_INT);
+        $stmt->bindValue(':date_time', $date_time, PDO::PARAM_STR);
+
+        if ($stmt->execute()) {
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $results;
+        }
     }
 
 }
