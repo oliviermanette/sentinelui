7c5c575d1cf36eb8a2efcb75160f761b6740ff37
Lirone <lirone@flod.ai>
2020-02-21 12:29:43 +0100

    Changement mot de passe deseign email and form

--- App/Mail.php
+++ App/Mail.php
@@ -38,7 +38,7 @@ class Mail
     try {
       $response = $sendgrid->send($email);
       print $response->statusCode() . "\n";
-      print_r($response->headers());
+      //print_r($response->headers());
       print $response->body() . "\n";
     } catch (\Exception $e) {
       echo 'Caught exception: ',  $e->getMessage(), "\n";
