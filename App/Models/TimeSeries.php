61f30945407b6a4c07594f023ab0fafa3454acd4
lirone <lirone.samoun@gmail.com>
2020-05-12 17:18:37 +0200

    Add new function and remove useless

--- App/Models/TimeSeries.php
+++ App/Models/TimeSeries.php
@@ -22,13 +22,10 @@ class TimeSeries extends \Core\Model
     private $structure_id;
     private $date_time;
     private $dataType_id;
-
     private $listPeakArr;
     private $listSubspectreArr;
-
     private $timeSerieArr;
     private $amplitudeArr;
-
     private $isCreated; //If timeSeries has been filed with data
 
 
@@ -45,120 +42,54 @@ class TimeSeries extends \Core\Model
         $this->listSubspectreArr = array();
         $this->listPeakArr = array();
         $this->timeSerieArr = array();
-        //$this->amplitudeArr = array();
-
         $this->isCreated = false;
-
-        //Init Data type
-        $recordManager = new RecordManager();
-        $this->dataType_id = $recordManager->getDataTypeIdFromName("spectre");
-    }
-
-    public function getId()
-    {
-        return $this->id;
     }
 
 
     /**
-     * get the five subspectre data that constitute a full spectre
+     * From a hex spectre data
      *
-     * @return array array which contain the data in hexa of each subspectre
-     */
-    public function getAllSubspectre()
-    {
-        return $this->listSubspectreArr;
-    }
-
-    /**
-     * get all the peak that have this time serie
-     *
-     * @return array array which contain all the peak (valX and valY)
-     */
-    public function getAllPeaks()
-    {
-        return $this->listPeakArr;
-    }
-
-    public function getTimeSerieData()
-    {
-        return $this->timeSerieArr;
-    }
-
-    /**
-     * replace list of peaks with a new list
-     * @param array @peakArr : news peaks
-     *
+     * @param object $hexSpectreData array data that contain all the info needed for building the timeserie
      * @return void
      */
-    public function setPeaks($peakArr)
+    public function createFromMsg($message)
     {
-        return $this->listPeakArr = $peakArr;
+        $date_time = $message->msgDecoded["dateTime"];
+        $this->date_time = $date_time;
+        $timestamp = strtotime($date_time);
+        $this->timestamp = $timestamp;
+        $structure_name = $message->structureName;
+        $this->structure_name = $structure_name;
+        $site_name = $message->site;
+        $this->site_name = $site_name;
+        $axisX_freq = $message->msgDecoded["min_freq"];
+        $resolution = $message->msgDecoded["resolution"];
+        $hexSpectreData = $message->msgDecoded["spectre_msg_hex"];
+
+
+        //Loop over the subspectre data values (hex format)
+        for ($j = 2; $j < intval(strlen(strval($hexSpectreData))); $j += 2) {
+            //We need to analyse two by two
+            $data_amplitude_j_hex = substr($hexSpectreData, $j, 2);
+            //Convert hexa value to decimal
+            $data_amplitude_j_dec = Utilities::hex2dec($data_amplitude_j_hex, $signed = false);
+            //From the decimal value, compute the power of the amplitude
+            $axisY_amplitude = Utilities::accumulatedTable32($data_amplitude_j_dec);
+
+            //Create a new peak
+            $peak = new Peak($axisX_freq, $axisY_amplitude);
+            //Put it to the list
+            array_push($this->listPeakArr, $peak);
+            $dataValueTmpArr = array("x" => $axisX_freq, "y" => $axisY_amplitude);
+            array_push($this->timeSerieArr, $dataValueTmpArr);
+
+            $axisX_freq = $axisX_freq + $resolution;
+        }
+
+        $this->isCreated = true;
     }
 
-    /**
-     * Find the first X peaks from the spectre
-     *
-     * @param array $peakArr array data which contain all the peak ([valX, valYH])
-     * @param int $nb first nbre peak to find in the spectre
-     * @param float $thresh_high difference of at least thresh_hig% between the max and lower values
-     * to be considered a peak.
-     * @param float $thresh_low limits peaks to trhesh_low% in amplitude of the largest peak.
-     * @return array array which contain all the peak (valX and valY)
-     */
-    public function findPeaks($peaksArr, $nb, $thresh_high = 0.25, $thresh_low = 0.05)
-    {
-        //init vars
-        $peaksResult = new PeaksList();
-        //First value to init
-        $fltXvalFreq = $peaksArr[0]->getValX();
-        $fltYvalAmplitude = $peaksArr[0]->getValY();
-        // init variables
-        $fltLocalMax = $fltYvalAmplitude;
-        $intCurrentPeakPos = $fltXvalFreq;
-        $lblSearch4Peak = true;
-        //On parcoure le tableau des peaks
-        for ($i = 1; $i < count($peaksArr); $i++) {
-            $fltXvalFreq = $peaksArr[$i]->getValX();
-            $fltYvalAmplitude = $peaksArr[$i]->getValY();
 
-
-            if ($fltYvalAmplitude > $fltLocalMax) {
-                $fltLocalMax = $fltYvalAmplitude;
-                $intCurrentPeakPos = $fltXvalFreq;
-                $lblSearch4Peak = true;
-            }
-            if ($lblSearch4Peak) {
-                if ($fltYvalAmplitude <= $thresh_high * $fltLocalMax) {
-                    $peaksResult->setNewPeak($intCurrentPeakPos, $fltLocalMax);
-                    //reset local peak to current position
-                    $fltLocalMax = $fltYvalAmplitude;
-                    $intCurrentPeakPos = $fltXvalFreq;
-                    $lblSearch4Peak = false;
-                }
-            }
-        }
-        // remove smallest Peaks under $thresh_low
-        $biggestPeak = $peaksResult->getLargestPeakAmplitude();
-        $NbPeaksTotal = $peaksResult->getNumber();
-
-        for ($i = 0; $i < $peaksResult->getNumber(); $i++) {
-            if (($biggestPeak * $thresh_low) < $peaksResult->getPeakSize($i))
-                $peaksResult->removePeak($i);
-        }
-        $IntDiff = 0;
-        if ($peaksResult->getNumber() <= $nb) {
-            return $peaksResult->getArray();
-        } else {
-            $IntDiff = $peaksResult->getNumber() - $nb;
-        }
-        // remove smallest peak to get $nb peaks.
-        for ($i = 0; $i < $IntDiff; $i++) {
-            $lIdxToRemove = $peaksResult->getSmallestPeakIndex();
-            $peaksResult->removePeak($lIdxToRemove);
-        }
-        return $peaksResult->getArray();
-    }
 
     /**
      * From a spectre Array data, create the time serie object
@@ -238,55 +169,89 @@ class TimeSeries extends \Core\Model
         foreach ($this->listPeakArr as $peak) {
             $axisX_freq = $peak->getValX();
             $axisY_amplitude = $peak->getValY();
-
-            echo "[" . $peak->getValX() . ", " . $peak->getValY() . "]" . "\n";
             //Insert data to DB
-            //TimeSeries::insertTimeSeriesData($this->record_id, $this->structure_id, $this->sensor_id, $this->date_time, $axisX_freq, $axisY_amplitude);
+            TimeSeriesManager::insertTimeSeriesData($this->record_id, $this->structure_id, $this->sensor_id, $this->date_time, $axisX_freq, $axisY_amplitude);
         }
-
         return true;
     }
 
+
     /**
-     * Insert Time Serie Data to Database
+     * Create a Json file from the timeserie data for the input of Sentive AI (specific format)
      *
-     * @param int $record_id
-     * @param int $structure_id
-     * @param int $sensor_id
-     * @param int $date_time
-     * @param float $valueX x axis (frequency of the peak)
-     * @param float $valueY y axis (amplitude of the peak)
-     * @return true if the object has been saved correctly
+     * @return json array which contain the input data for Sentive AI
      */
-    private static function insertTimeSeriesData($record_id, $structure_id, $sensor_id, $date_time, $valueX, $valueY)
+    public function parseForSentiveAi()
     {
-        $db = static::getDB();
+        $X_arr = array();
+        $Y_arr = array();
+        $dataArr = array();
+        if (!empty($this->timeSerieArr)) {
+            foreach ($this->timeSerieArr as $timeserieArr) {
+                $X = $timeserieArr['x'];
+                $Y = $timeserieArr['y'];
+                array_push($X_arr, intval($X));
+                array_push($Y_arr, intval($Y));
+            }
+            //Set datetime attribute
+            $dataArr["datetime"] = $this->timestamp;
+            //Set ValueX attribute
+            $dataArr["ValueX"] = $X_arr;
+            //Set ValueY attribute
+            $dataArr["ValueY"] = $Y_arr;
+            //Set networkId attribute
+            $dataArr["FKNetworkID"] = intval($this->networkId);
 
-        $sql = "INSERT INTO timeseries(record_id, structure_id, sensor_id, dataType_id, date_time, valueX, valueY)
-        SELECT :record_id, :structure_id, :sensor_id, (SELECT id FROM dataType WHERE nom = 'spectre'), :date_time, :valueX, :valueY
-        WHERE NOT EXISTS (
-            SELECT * FROM timeseries WHERE record_id = :record_id AND valueX = :valueX AND valueY = :valueY
-        ) LIMIT 1
-        ";
-
-        $stmt = $db->prepare($sql);
-        $stmt->bindValue(':valueX', $valueX, PDO::PARAM_STR);
-        $stmt->bindValue(':valueY', $valueY, PDO::PARAM_STR);
-        $stmt->bindValue(':date_time', $date_time, PDO::PARAM_STR);
-        $stmt->bindValue(':sensor_id', $sensor_id, PDO::PARAM_INT);
-        $stmt->bindValue(':structure_id', $structure_id, PDO::PARAM_INT);
-        $stmt->bindValue(':record_id', $record_id, PDO::PARAM_INT);
-        $stmt->execute();
-
-        $ok = $stmt->execute();
-
-        $db = null;
-
-        if ($ok) {
-            return true;
+            return json_encode($dataArr);
+        } else {
+            return null;
         }
-        return false;
     }
+
+
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    /**
+     * get the five subspectre data that constitute a full spectre
+     *
+     * @return array array which contain the data in hexa of each subspectre
+     */
+    public function getAllSubspectre()
+    {
+        return $this->listSubspectreArr;
+    }
+
+    /**
+     * get all the peak that have this time serie
+     *
+     * @return array array which contain all the peak (valX and valY)
+     */
+    public function getAllPeaks()
+    {
+        return $this->listPeakArr;
+    }
+
+    public function getTimeSerieData()
+    {
+        return $this->timeSerieArr;
+    }
+
+    /**
+     * replace list of peaks with a new list
+     * @param array @peakArr : news peaks
+     *
+     * @return void
+     */
+    public function setPeaks($peakArr)
+    {
+        return $this->listPeakArr = $peakArr;
+    }
+
 
     public function setNetworkId($networkId)
     {
@@ -306,28 +271,67 @@ class TimeSeries extends \Core\Model
         return $this->date_time;
     }
 
-    public function parseForSentiveAi()
+    /**
+     * Find the first X peaks from the spectre
+     *
+     * @param array $peakArr array data which contain all the peak ([valX, valYH])
+     * @param int $nb first nbre peak to find in the spectre
+     * @param float $thresh_high difference of at least thresh_hig% between the max and lower values
+     * to be considered a peak.
+     * @param float $thresh_low limits peaks to trhesh_low% in amplitude of the largest peak.
+     * @return array array which contain all the peak (valX and valY)
+     */
+    public function findPeaks($peaksArr, $nb, $thresh_high = 0.25, $thresh_low = 0.05)
     {
-        $X_arr = array();
-        $Y_arr = array();
-        $dataArr = array();
-        if (!empty($this->timeSerieArr)) {
-            foreach ($this->timeSerieArr as $timeserieArr) {
-                $X = $timeserieArr['x'];
-                $Y = $timeserieArr['y'];
-                array_push($X_arr, intval($X));
-                array_push($Y_arr, intval($Y));
+        //init vars
+        $peaksResult = new PeaksList();
+        //First value to init
+        $fltXvalFreq = $peaksArr[0]->getValX();
+        $fltYvalAmplitude = $peaksArr[0]->getValY();
+        // init variables
+        $fltLocalMax = $fltYvalAmplitude;
+        $intCurrentPeakPos = $fltXvalFreq;
+        $lblSearch4Peak = true;
+        //On parcoure le tableau des peaks
+        for ($i = 1; $i < count($peaksArr); $i++) {
+            $fltXvalFreq = $peaksArr[$i]->getValX();
+            $fltYvalAmplitude = $peaksArr[$i]->getValY();
+
+
+            if ($fltYvalAmplitude > $fltLocalMax) {
+                $fltLocalMax = $fltYvalAmplitude;
+                $intCurrentPeakPos = $fltXvalFreq;
+                $lblSearch4Peak = true;
+            }
+            if ($lblSearch4Peak) {
+                if ($fltYvalAmplitude <= $thresh_high * $fltLocalMax) {
+                    $peaksResult->setNewPeak($intCurrentPeakPos, $fltLocalMax);
+                    //reset local peak to current position
+                    $fltLocalMax = $fltYvalAmplitude;
+                    $intCurrentPeakPos = $fltXvalFreq;
+                    $lblSearch4Peak = false;
+                }
             }
         }
-        //Set datetime attribute
-        $dataArr["datetime"] = $this->timestamp;
-        //Set ValueX attribute
-        $dataArr["ValueX"] = $X_arr;
-        //Set ValueY attribute
-        $dataArr["ValueY"] = $Y_arr;
-        //Set networkId attribute
-        $dataArr["FKNetworkID"] = intval($this->networkId);
+        // remove smallest Peaks under $thresh_low
+        $biggestPeak = $peaksResult->getLargestPeakAmplitude();
+        $NbPeaksTotal = $peaksResult->getNumber();
 
-        return json_encode($dataArr);
+        for ($i = 0; $i < $peaksResult->getNumber(); $i++) {
+            if (($biggestPeak * $thresh_low) < $peaksResult->getPeakSize($i))
+                $peaksResult->removePeak($i);
+        }
+        $IntDiff = 0;
+        if ($peaksResult->getNumber() <= $nb) {
+            return $peaksResult->getArray();
+        } else {
+            $IntDiff = $peaksResult->getNumber() - $nb;
+        }
+        // remove smallest peak to get $nb peaks.
+        for ($i = 0; $i < $IntDiff; $i++) {
+            $lIdxToRemove = $peaksResult->getSmallestPeakIndex();
+            $peaksResult->removePeak($lIdxToRemove);
+        }
+        return $peaksResult->getArray();
     }
 }
